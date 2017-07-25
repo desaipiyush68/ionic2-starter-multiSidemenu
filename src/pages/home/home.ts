0601a210f93a0357c200a6896ed3bf0b78c92dab
desaipiyush68 <desaipiyush402@gmail.com>
2017-07-25 17:27:20 +0100

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, MenuController  } from 'ionic-angular';
+import { Pageone } from '../pageone/pageone';
+import { Pagetwo } from '../pagetwo/pagetwo';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(
+        public navCtrl: NavController,
+        public menu: MenuController) {
 
+        menu.enable(false, 'Pageone');
+        menu.enable(false, 'Pagetwo');
+        menu.enable(false, 'ContactPage');
+        menu.enable(false, 'AboutPage');
+        menu.enable(true, 'HomePage');
+
+    }
+  
+  PageOne() {
+      this.navCtrl.setRoot(Pageone);
+  }
+
+  PageTwo() {
+      this.navCtrl.setRoot(Pagetwo);
   }
 
 }
